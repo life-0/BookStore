e353940c391fef66bc642610e3b396572728a9a1
life-0 <1471925853@qq.com>
2021-07-05 09:57:58 +0800

    解决上传文件为空的bug

--- out/artifacts/BookStore_war_exploded/WEB-INF/jsp/upAndDown.jsp
+++ out/artifacts/BookStore_war_exploded/WEB-INF/jsp/upAndDown.jsp
@@ -8,10 +8,80 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>$Title$</title>
+    <title>用户登录页面</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <script type="text/javascript" src="${pageContext.request.contextPath}/static/JQuery/jquery-3.3.1.min.js"></script>
+    <%--使用Bootstrap美化界面--%>
+    <link href="${pageContext.request.contextPath}/static/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="${pageContext.request.contextPath}/static/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+    <script type="application/javascript">
+        window.onload = function () {
+            let result=getCookie("file")
+            if (result!==""){
+                window.alert(result);
+                delCookie("file");
+            }
+
+        }
+        //获得cookie值
+        function getCookie(name) {
+            let strcookie = document.cookie;//获取cookie字符串
+            let arrcookie = strcookie.split("; ");//分割
+            //遍历匹配
+            for (let i = 0; i < arrcookie.length; i++) {
+                let arr = arrcookie[i].split("=");
+                if (arr[0] === name) {
+                    return arr[1];
+                }
+            }
+            return "";
+        }
+
+        //删除cookie
+        function delCookie(name) {//删除cookie
+            let exp = new Date();
+            exp.setTime(exp.getTime() - 1);
+            let cval = getCookie(name);
+            if (cval != null) {
+                document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+            }
+        }
+    </script>
 </head>
 <body>
-<div>
+<div align="center">
+    <h1>UploadAndDownLoad</h1>
+</div>
+
+
+<div class="col-md-12">
+    <div style="text-align: center">
+        <form class="form-horizontal" action="${pageContext.request.contextPath}/upload"
+              enctype="multipart/form-data" method="post" >
+            <div class="form-group" >
+                <div class="col-sm-offset-2 col-sm-8 " style="display: inline">
+                    <input type="file" name="file" style="display: inline">
+                    <input type="submit"  value="upload">
+                </div>
+            </div>
+        </form>
+        <form action="${pageContext.request.contextPath}/download" enctype="multipart/form-data" method="post">
+            <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-8">
+                    <input type="submit" value="download">
+                </div>
+            </div>
+        </form>
+    </div>
+
+</div>
+</body>
+
+<%--<div>
     <div align="center">
         <form action="${pageContext.request.contextPath}/upload" enctype="multipart/form-data" method="post">
             <input type="file" name="file">
@@ -26,5 +96,5 @@
         </form>
     </div>
 </div>
-</body>
+</body>--%>
 </html>
--- src/main/java/com/life/Controller/FileController.java
+++ src/main/java/com/life/Controller/FileController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
@@ -22,59 +23,65 @@ public class FileController {
     //批量上传CommonsMultipartFile则为数组即可
 
     @RequestMapping("/file")
-    public String fileWeb(){
+    public String fileWeb() {
         return "/upAndDown";
     }
+
     /*
      * 采用file.Transit 来保存上传的文件
      */
     @RequestMapping("/upload")
-    public String  fileUpload2(@RequestParam("file") CommonsMultipartFile file, HttpServletRequest request) throws IOException {
+    public String fileUpload(@RequestParam("file") CommonsMultipartFile file,
+                             HttpServletResponse response, HttpServletRequest request)
+            throws IOException {
+        if (file.isEmpty ()) {
+            Cookie cookie = new Cookie ("file", "文件为空请重新上传文件!");
+            response.addCookie (cookie);
+        } else {
+            //上传路径保存设置
+            String path = request.getServletContext ().getRealPath ("/upload");
+            File realPath = new File (path);
+            if (!realPath.exists ()) {
+                realPath.mkdir ();
+            }
+            //上传文件地址
+            System.out.println ("上传文件保存地址：" + realPath);
 
-        //上传路径保存设置
-        String path = request.getServletContext().getRealPath("/upload");
-        System.out.println (request.getRequestURI());
-        File realPath = new File(path);
-        if (!realPath.exists()){
-            realPath.mkdir ();
+            //通过CommonsMultipartFile的方法直接写文件（注意这个时候）
+            file.transferTo (new File (realPath + "/" + file.getOriginalFilename ()));
         }
-        //上传文件地址
-        System.out.println("上传文件保存地址："+realPath);
-
-        //通过CommonsMultipartFile的方法直接写文件（注意这个时候）
-        file.transferTo(new File(realPath +"/"+ file.getOriginalFilename()));
-
         return "redirect:/file";
     }
-        //下载
-    @RequestMapping(value="/download")
-    public String downloads(HttpServletResponse response , HttpServletRequest request) throws Exception{
-        //要下载的图片地址
-        String  path = request.getServletContext().getRealPath("/WEB-INF");
-        String  fileName = "test.txt";
-        //1、设置response 响应头
-        response.reset(); //设置页面不缓存,清空buffer
-        response.setCharacterEncoding("UTF-8"); //字符编码
-        response.setContentType("multipart/form-data"); //二进制传输数据
-        //设置响应头
-        response.setHeader("Content-Disposition",
-                "attachment;fileName="+ URLEncoder.encode(fileName, "UTF-8"));
-        //创建文件实例
-        File file = new File(path,fileName);
-        //2、 读取文件--输入流
-        InputStream input=new FileInputStream(file);
-        //3、 写出文件--输出流
-        OutputStream out = response.getOutputStream();
 
-        byte[] buff =new byte[1024];
-        int index=0;
+    //下载
+    @RequestMapping(value = "/download")
+    public String downloads(HttpServletResponse response, HttpServletRequest request) throws Exception {
+        //要下载的图片地址
+        String path = request.getServletContext ().getRealPath ("/WEB-INF");
+        String fileName = "test.txt";
+        //1、设置response 响应头
+        response.reset (); //设置页面不缓存,清空buffer
+        response.setCharacterEncoding ("UTF-8"); //字符编码
+        response.setContentType ("multipart/form-data"); //二进制传输数据
+        //设置响应头
+        response.setHeader ("Content-Disposition",
+                "attachment;fileName=" + URLEncoder.encode (fileName, "UTF-8"));
+        //创建文件实例
+        File file = new File (path, fileName);
+        //2、 读取文件--输入流
+        InputStream input = new FileInputStream (file);
+        //3、 写出文件--输出流
+        OutputStream out = response.getOutputStream ();
+
+        byte[] buff = new byte[1024];
+        int index = 0;
         //4、执行 写出操作
-        while((index= input.read(buff))!= -1){
-            out.write(buff, 0, index);
-            out.flush();
+        while ((index = input.read (buff)) != -1) {
+            out.write (buff, 0, index);
+            out.flush ();
         }
-        out.close();
-        input.close();
+        out.close ();
+        input.close ();
         return null;
     }
 }
